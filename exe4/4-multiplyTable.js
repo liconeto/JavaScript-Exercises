0bf38c4f878d45a13d99e1e280d86cfd1051ce54
liconeto <benicio.neto9@gmail.com>
2023-01-31 17:58:16 -0300

    Correction in language and development, exe04

--- exe4/4-multiplyTable.js
+++ exe4/4-multiplyTable.js
@@ -1,23 +1,30 @@
 let numInt = document.querySelector("input#numInt");
-let tabuada = document.querySelector("table#tabuada");
+let multtable = document.querySelector("div#multtable");
 let res = document.querySelector("div#res");
 
 function multiply() {
   let num1 = Number(numInt.value);
 
-  let tabela = document.createElement("table");
-  let cabecalho = document.createElement("thead");
-  let corpo = document.createElement("tbody");
-
-  tabela.appendChild(cabecalho);
-  tabela.appendChild(corpo);
-
-  document.getElementById("tabuada").appendChild(tabela);
+  let tableM = document.getElementById("multtable");
+  tableM.innerHTML += [
+    "<table>" +
+      "<thead>" +
+      "<tr>" +
+      "<th>Operator</th>" +
+      "<th>Multiplicator</th>" +
+      "<th>Result</th>" +
+      "</tr>" +
+      "</thead>" +
+      "<tbody>",
+  ];
   for (i = 1; i < 11; i += 1) {
-    let itemLista = document.createElement("td");
-    itemLista.text = `${i}, `;
-    alert(`Letra =${i} da palavra: ${i * num1}`);
-    lista.appendChild(itemLista);
-    res.innerHTML = "";
+    tableM.innerHTML += [
+      `<tr>
+         <td>${num1} * </td>
+         <td> ${i} = </td>
+         <td>${num1 * i}</td>
+       </tr>`,
+    ];
   }
+  tableM.innerHTML += ["</tbody>" + "</table>"].join("\n");
 }
