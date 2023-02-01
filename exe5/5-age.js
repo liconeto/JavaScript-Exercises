c7bbe1c1f2774fd4f5dd3a90aabbff533fd63292
liconeto <benicio.neto9@gmail.com>
2023-02-01 10:38:03 -0300

    conclusion of the exercise number 5, Legal Age

--- exe5/5-age.js
+++ exe5/5-age.js
@@ -5,11 +5,12 @@ function verifiyAge(numInt) {
   let num1 = Number(numInt.value);
   let aviso = document.createElement("p");
 
-  if (num1 >= 18) {
-    aviso.innerText = "congratulation, Your has  the legal age";
-  } else {
-    aviso.innerText =
-      "Alert!. Your won't the legal age, returns with your responsable!";
+  if (!num1 == 0) {
+    if (num1 >= 18) {
+      aviso.innerText = `Congratulation, You has ${num1}, is the legal age`;
+    } else {
+      aviso.innerText = `Alert!. You has ${num1}, is not the legal age, returns with your responsable!`;
+    }
   }
   document.getElementById("respCheck").appendChild(aviso);
 }
