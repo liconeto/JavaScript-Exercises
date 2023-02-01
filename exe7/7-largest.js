e86617386ab70167d78945510102ef6ab449756b
liconeto <benicio.neto9@gmail.com>
2023-02-01 17:59:01 -0300

    exercise 7 in development

---
+++ exe7/7-largest.js
@@ -0,0 +1,10 @@
+let inputsN = document.getElementsByName("num[]");
+
+let respCheck = document.querySelector("div#respCheck");
+
+function largest() {
+  console.log(inputsN.values);
+  alert(inputsN.item);
+}
+
+largest();
